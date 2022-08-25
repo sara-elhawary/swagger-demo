e65231cb62a6582b36d8dd6564ed369a7e6b3e9b
sara-elhawary <sarah.elhwarii92gmail.com>
2022-08-25 09:42:27 +0200

    add get todo by id endpoint

--- routes/toDosRoutes.js
+++ routes/toDosRoutes.js
@@ -4,9 +4,11 @@ import express from "express";
 const router = express.Router();
 const idLength = 8;
 
-router.get("/", (req, res) => {
-  let todos = req.app.get("todos");
-  return res.send({ todos });
+router.get("/", async (req, res) => {
+  let todos = req.app.db.data.todos;
+  // await req.app.db.read();
+  console.log(todos);
+  return res.send(todos);
 });
 
 router.post("/", async (req, res) => {
@@ -23,4 +25,23 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.get("/:id", (req, res) => {
+  const given_id = req.params.id;
+  let todos = req.app.db.data.todos;
+
+  const wanted_todo = todos.find((todo) => todo.id == given_id);
+  return res.send(wanted_todo);
+});
+
+// router.delete("/:id", async (req, res) => {
+//   const given_id = req.params.id;
+//   const todos = req.app.db.data.todos;
+//   try {
+//     todos.delete(todos.findIndex((todo) => todo.id == given_id));
+//     await req.app.db.write();
+//     return res.status(200).send("todo is successfullt deleted");
+//   } catch (err) {
+//     return res.status(500).send({ err });
+//   }
+// });
 export default router;
